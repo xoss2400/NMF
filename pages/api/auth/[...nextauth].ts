4c90d4a9a3d06440ae789ce1f7256116578b449d
John McGowan <jmcgowan5g@gmail.com>
2025-09-30 04:22:46 -0400

    still tryin

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -20,7 +20,7 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-      authorization: "https://accounts.spotify.com/authorize?scope=user-top-read",
+      authorization: 'https://accounts.spotify.com/authorize?scope=user-top-read',
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
