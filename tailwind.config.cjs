2192192e061a0bbc63350b348a5af28d5686cff3
John McGowan <jmcgowan5g@gmail.com>
2025-09-30 05:40:23 -0400

    maybe? please?

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{ts,tsx}',        // App router files
+    './pages/**/*.{ts,tsx}',      // Optional, if you also have Pages
+    './components/**/*.{ts,tsx}', // All components
   ],
   theme: {
     extend: {},
