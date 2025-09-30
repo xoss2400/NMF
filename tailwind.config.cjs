d7032873c6b12b3fe877efb716446ac45d9dfcfd
John McGowan <jmcgowan5g@gmail.com>
2025-09-30 03:57:06 -0400

    fixed tailwindcss issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,11 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-export default config
