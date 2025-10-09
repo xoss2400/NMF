588288731cf0735926205c009a54f45407bd1044
John McGowan <jmcgowan5g@gmail.com>
2025-10-09 19:26:18 -0400

    config help

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    // For Tailwind v4+, the recommended PostCSS plugin package is '@tailwindcss/postcss'
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
