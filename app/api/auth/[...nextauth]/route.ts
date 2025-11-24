976f8f7e69e6feddbbdbe2e3740b0b1ba4dc1255
John McGowan <jmcgowan5g@gmail.com>
2025-11-24 14:38:59 -0500

    try3

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 // app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
